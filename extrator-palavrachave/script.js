f10f3a08cbb3907185846c4140bfa2cdc3417834
Iana Ekuni Gonçalves <iana.goncalves@escola.pr.gov.br>
2025-06-17 08:00:17 -0300

    a

--- extrator-palavrachave/script.js
+++ extrator-palavrachave/script.js
@@ -3,5 +3,31 @@ const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 
 function mostraPalavrasChave() {
-    alert('fui clicado!');
+    const texto = document.querySelector('#entrada-de-texto').value;
+    const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavrasChave = processaTexto(texto);
+
+    campoResultado.textContent = palavrasChave.join(", ");
+}
+
+function processaTexto(texto){
+    let palavras = texto.split(/\P{L}+/u/);
+    const frequencias = contaFrequencias(palavras);
+    let ordenadas = Object.keys(frequencias);
+    return palavras;
+}
+
+function contaFrequencias(palavras){
+    let frequencias = {};
+    for(let i of palavras){
+        frequencias[i] = 0;
+        for (let j of palavras){
+            if(i == j){
+                frequencias[i]++;
+            }
+        }
+    }
+
+    return frequencias;
+
 }
